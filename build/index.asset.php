c779042b7d2d1b89adb09fa3c9f9d8e497373278
Md. Mostak Shahid <mostak.shahid@gmail.com>
2025-05-15 02:21:43 +0600

    added dashboard

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-i18n'), 'version' => 'b58148f94ad251b23d0b');
+<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-i18n'), 'version' => 'eed89a1f0043d984c03e');
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
 
 export default function Dashboard() {
     return (
         <div className="mos-faqs-settings">
             <div className="container-fluid p-0">
+                <div className="card mt-0 mb-3 rounded-0">
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-lg-7">
+                                    <h6 className="card-subtitle mb-2 text-body-secondary">Hello Admin!</h6>
+                                    <h2 className="card-title">Welcome to Plugin Name <span class="badge text-bg-light">Light</span></h2>
+                                    <p className="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus illum sed, facilis consequuntur ipsa consequatur dolores nisi, sit vel est earum autem ipsam voluptas odit vero quisquam natus dolorem! Maxime tempora deserunt dolorem exercitationem animi doloremque enim maiores distinctio non!</p>
+                                    <button type="button" class="btn btn-primary">Primary</button>
+                                    <button type="button" class="btn">Base class</button>
+                            </div>
+                            <div className="col-lg-5"><div className="d-flex align-items-center justify-content-center w-100 h-100 bg-secondary text-white">Image</div></div>
+                        </div>
+                        
+                    </div>
+                </div>
                 <div className="row">
                     <div className="col-lg-8 mb-4 mb-lg-0">
                         <div className="card mt-0 mb-3 rounded-0">
                             <div className="card-header">
-                                Dashboard
+                                Shortcode
                             </div>
                             <div className="card-body">
                                 <h5 className="card-title">Card title</h5>
@@ -18,33 +32,70 @@ export default function Dashboard() {
                                 <a href="#" className="card-link">Another link</a>
                             </div>
                         </div>
+                        
                         <div className="card mt-0 rounded-0">
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="card-link">Card link</a>
-                                <a href="#" className="card-link">Another link</a>
+                                <div className="row">
+                                    <div className="col-lg-7">
+                                        <h3 className="card-title">Limitless Design With Spectra Pro!</h3>
+                                        <p className="card-text">Utilize advanced blocks, extensions, and premium features to create a websites that stands out!</p>
+                                        <a href="#" className="card-link">Card link</a>
+                                        <a href="#" className="card-link">Another link</a>
+                                    </div>
+                                    <div className="col-lg-5">
+                                        <div className="d-flex align-items-center justify-content-center w-100 h-100 bg-secondary text-white">Image</div>
+                                    </div>
+                                </div>
+                                
                             </div>
                         </div>
                     </div>
                     <div className="col-lg-4">
                         <div className="card mt-0 mb-3 rounded-0">
+                            <div className="card-header">
+                                Extend Your Website
+                            </div>
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" className="card-link">Card link</a>
-                                <a href="#" className="card-link">Another link</a>
+                                <div className="row">
+                                    <div className="col-auto">
+                                        <div style={{width:'60px', height:'60px', background:'#eaeaea'}}></div>
+                                    </div>
+                                    <div className="col">
+                                        <h5>title</h5>
+                                        <p className="intro">Lorem ipsum dolor sit amet.</p>
+                                        <div className="action"><a href="#" className="card-link">Card link</a></div>
+                                    </div>
+                                </div><div className="row">
+                                    <div className="col-auto">
+                                        <div style={{width:'60px', height:'60px', background:'#eaeaea'}}></div>
+                                    </div>
+                                    <div className="col">
+                                        <h5>title</h5>
+                                        <p className="intro">Lorem ipsum dolor sit amet.</p>
+                                        <div className="action"><a href="#" className="card-link">Card link</a></div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="card rounded-0">
-                            <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
-                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <div className="card mt-0 mb-3 rounded-0">
+                            <div className="card-body">                                
+                                <h4 className="card-title">VIP Priority Support</h4>
+                                <p className="card-text">Faster and exclusive support service designed for VIP assistance and benefits.</p>
                                 <a href="#" className="card-link">Card link</a>
-                                <a href="#" className="card-link">Another link</a>
+                            </div>
+                        </div>
+                        <div className="card mt-0 mb-3 rounded-0">
+                            <div className="card-body">                                
+                                <h4 className="card-title">Join the Community</h4>
+                                <p className="card-text">Got a question about the plugin, want to share your awesome project or just say hi? Join our wonderful community!</p>
+                                <a href="#" className="card-link">Card link</a>
+                            </div>
+                        </div>
+                        <div className="card mt-0 mb-3 rounded-0">
+                            <div className="card-body">                                
+                                <h4 className="card-title">Rate Us</h4>
+                                <p className="card-text">We love to hear from you, we would appreciate every single review.</p>
+                                <a href="#" className="card-link">Card link</a>
                             </div>
                         </div>
                     </div>
